4c8d76b01e8cce510953b71cde13d62123bed631
matowang <wmatthew123@gmail.com>
2019-10-02 14:19:32 +0800

    added egirlmode toggle command

--- mybot.js
+++ mybot.js
@@ -1,18 +1,41 @@
 const client = require("./lib/discord-client.js");
+const config = require("./config.json");
+
+let eGirlModeOn = false;
 
 client.on("ready", () => {
     console.log("I am ready!");
 });
 
-client.on("message", (message) => {
-    if (message.author.bot) return;
+client.on("message", (msg) => {
+    if (msg.author.bot) return;
     console.log("i hear u");
+
+    if (msg.content.startsWith(config.prefix)) {
+        const command = msg.content.subStr(config.prefix.length);
+        switch (command.toLowerCase()) {
+            case "toggle egirlmode":
+                eGirlModeOn = !eGirlModeOn;
+                if (!eGirlModeOn)
+                    msg.channel.send(":anguished:");
+                break;
+            default:
+                msg.channel.send("Nani ><");
+        }
+    }
+
+    flirt(msg);
+});
+
+const flirt = (msg) => {
+    if (!eGirlModeOn) return;
+
     if (Math.random() < .5) {
-        message.channel.send("uwu");
+        msg.channel.send("uwu");
         console.log("uwu")
     }
     else {
-        message.channel.send("owo");
+        msg.channel.send("owo");
         console.log("owo")
     }
-});
+}
